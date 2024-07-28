84ed85afc120c791515a70dcf72a1d05f1b3c89a
unknown <missingideas@gmail.com>
2024-07-28 14:49:29 -0400

    Fix axis

--- d3_viz.js
+++ d3_viz.js
@@ -2,8 +2,8 @@ async function loadSceneOne() {
     const data = await d3.csv("https://flunky.github.io/cars2017.csv");
     const svg = d3.select("svg").append("g").attr("transform", "translate(50,50)");
 
-    const x_scale = d3.scaleLinear().domain([10, 150]).range([0, 500]);
-    const y_scale = d3.scaleLinear().domain([10, 150]).range([500, 0]);
+    const x_scale = d3.scaleLog().domain([10, 150]).range([0, 500]);
+    const y_scale = d3.scaleLog().domain([10, 150]).range([500, 0]);
     const color_band = d3.scaleOrdinal().domain(["Gasoline", "Diesel", "Electricity"]).range(["red", "green", "yellow"])
 
     const xAxis = d3.axisBottom(x_scale)
@@ -15,7 +15,7 @@ async function loadSceneOne() {
         .tickFormat(d3.format("~s"));
 
     const xAxisG = svg.append("g")
-        .attr("transform", "translate(0,200)")
+        .attr("transform", "translate(0,500)")
         .call(xAxis);
     xAxisG.selectAll("path")
         .attr("stroke", "currentColor")
