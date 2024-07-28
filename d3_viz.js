4c8539c01b0884c25327538f6af1a40480a0e3eb
unknown <missingideas@gmail.com>
2024-07-28 15:00:11 -0400

    Fix issue

--- d3_viz.js
+++ d3_viz.js
@@ -2,8 +2,8 @@ async function loadSceneOne() {
     const data = await d3.csv("https://flunky.github.io/cars2017.csv");
     const svg = d3.select("svg").append("g").attr("transform", "translate(50,50)");
 
-    const x_scale = d3.scaleLog().domain([0, 180]).range([0, 500]);
-    const y_scale = d3.scaleLog().domain([0, 180]).range([500, 0]);
+    const x_scale = d3.scaleLog().domain([10, 150]).range([0, 500]);
+    const y_scale = d3.scaleLog().domain([10, 150]).range([500, 0]);
     const color_band = d3.scaleOrdinal().domain(["Gasoline", "Diesel", "Electricity"]).range(["red", "green", "yellow"])
 
     const xAxis = d3.axisBottom(x_scale)
@@ -44,7 +44,7 @@ async function loadSceneOne() {
         .attr("class", "axis-title")
         .attr("transform", "rotate(-90)")
         .attr("y", 6)
-        .attr("dy", ".71em")
+        .attr("dy", ".6em")
         .style("text-anchor", "end")
         .attr("fill", "#5D6971")
         .text("Population)");
